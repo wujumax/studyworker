395efabc8a3aadf4d5b69922e1b77f8601626535
1399852582@qq.com <1399852582@qq.com>
2019-09-04 10:41:45 +0800

    add 微信小程序

---
+++ 微信小程序/pages/pages01/page01.js
@@ -0,0 +1,22 @@
+const app = getApp()
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    imgUrls: [
+      'http://hbimg.b0.upaiyun.com/1d3f617b893c4237a2ac179c478ac41415a26fdb42ce8-p9yG1u_fw658',
+'http://pic1.win4000.com/wallpaper/6/599ce1971aeaf.jpg',
+
+'http://www.ghost64.com/qqtupian/zixunImg/local/2014/07/04/1404441637189.jpg'
+    ]
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  }
+})
