25af14816a305d24625b2515ed16295fbc9c1a49
hllwld <2368773701@qq.com>
2019-09-04 14:12:54 +0800

    1.3

--- 微信小程序/pages/pages01/page01.js
+++ 微信小程序/pages/pages01/page01.js
@@ -6,18 +6,35 @@ Page({
    */
   data: {
     indexSelect: 0,
+    indexSelecttwo: 0,
     topics: [{
       id: 0,
       title: "运营"
     }, {
       id: 1,
       title: "行业"
-      }, {
-        id: 2,
-        title: "基础"
-      }]
+    }, {
+      id: 2,
+      title: "基础"
+    }],
+    topicstwo: [{
+      em: 0,
+      title: "运营"
+    }, {
+      em: 1,
+      title: "行业"
+    }, {
+      em: 2,
+      title: "基础"
+    }, {
+      em: 3,
+      title: "行业"
+    }, {
+      em: 4,
+      title: "基础"
+    }]
   },
-   clickTopItem: function (res) {
+  clickTopItem: function(res) {
     //获得传递过来的数据
     let id = res.currentTarget.dataset.yjs;
     //把index换成id
@@ -25,11 +42,19 @@ Page({
       indexSelect: id
     });
   },
+  clickTopItemtwo: function (res) {
+    //获得传递过来的数据
+    let em = res.currentTarget.dataset.yjs;
+    //把index换成id
+    this.setData({
+      indexSelecttwo: em
+    });
+  },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   }
 })
